2f51952fb44266f39b100383414f5f2bab4b7161
SMV <kraft088@gmail.com>
2023-01-08 20:39:22 +1100

    Add Example 4

--- Lecture_04/Example004_Fibonacci/Program.cs
+++ Lecture_04/Example004_Fibonacci/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//f(1) = 1
+//f(2) = 1
+//f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2)
+    return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    //System.Console.WriteLine(Fibonacci(i));
+    System.Console.WriteLine($"Fibonacci({i}) = {Fibonacci(i)}"); //Добавили вывод от какого числа рассчитывается.
+}
